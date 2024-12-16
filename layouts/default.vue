56311eb80479cfc7813f43c8d1ec3b61ae7aa339
Rob Beach <surfdemon@gmail.com>
2024-12-16 09:34:20 +0000

    Add: default layout and nav

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <nav>
+        <ul>
+            <li>
+                <nuxt-link to="/">Home</nuxt-link>
+            </li>
+            <li>
+                <nuxt-link to="/about">About</nuxt-link>
+            </li>
+            <li>
+                <nuxt-link to="/contact">Contact</nuxt-link>
+            </li>
+            <li>
+                <nuxt-link to="/portfolio">Portfolio</nuxt-link>   
+            </li>
+        </ul>
+    </nav>
+    <slot />
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
