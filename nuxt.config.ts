94eb6bf20c54a63860c11d50f94ae6a82ae99764
Rob Beach <surfdemon@gmail.com>
2024-12-21 18:27:39 +0000

    Maint: update config files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  modules:[
+    '@nuxtjs/google-fonts',
+    '@nuxtjs/tailwindcss',
+  ],
+  tailwindcss: { 
+      exposeConfig: true, 
+      viewer: true,
+  },
+  googleFonts:{
+    families: {
+      Inter: [400, 500, 600, 700],
+      'DM+Sans': [400, 500, 600, 700]
+    },
+    display: 'swap',
+    preload: true,
+    preconnect: true,
+    useStylesheet: true
+  },
+
+  compatibilityDate: '2024-12-19'
+});
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: [],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
+
