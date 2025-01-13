9b0858e6b6392d8d8147a55aa65e8838aa2ef829
Rob Beach <surfdemon@gmail.com>
2025-01-13 23:48:25 +0000

    Add: more content to FS4U page

--- pages/portfolio/freeslots4u.vue
+++ pages/portfolio/freeslots4u.vue
@@ -34,15 +34,29 @@
 
                 <div class=" justify-center items-center">
                     <p class="m-8 text-white">
-                        freeslots4u.com is one of the websites that I worked on while working at Atelm Ltd. 
+                        Freeslots4u.com is one of the websites that I worked on while working at Atelm Ltd. It was one of their main sites 
+                        andn sadly just about the only one that is still live. 
                         The site is a free slots site that offers free to play slots games. All the games have monthly leaderboards and 
-                        the site features a tournament system that allows competitions to be run on the site.  
-                        The site has been around for over 10 years. The site is monetized through advertising and affiliate links. 
+                        the site features a tournament system that allows competitions to be run on the site. The site also has a
+                        newsletter that is sent out to subscribers letting them know of the latest compotinions on the site and at the partner casinos. 
+                    </p>
+                    <p class="m-8 text-white">
+                        The site has been around for a long time and has gone through various itterations of design. All of its games were originally 
+                        Flash games. When Flash was depreciated, I co-ordinated with our graphics designer to recreate images for HTML5 versions 
+                        of the games and then hired freelance developers to convert all the games to HTML5 and create new HTML5 games. 
+                        I was responsible for testing the games and getting the games to load on the website. I also made it so that games would 
+                        only show to visitors from certain countries if the sponsoring casino didn't accept players from that country.
+                    </p>
+                    <p class="m-8 text-white">
+                        The site is monetized through advertising and affiliate links and casinos sponsored for games to be made with their branding on.
+                    </p>
+                    <p class="m-8 text-white">
                         I was responsible for everything from setting up the dedicated server to having content written for the site. 
                         Over the years, I have moved the site to different servers and have made changes to the site as needed. 
-                        This included moving it from a shared hosting account to a self managed virtual servers and then to a self 
-                        managed dedicated server. 
+                        This included things such as, moving it from a shared hosting account to a self managed virtual servers and then to a self 
+                        managed dedicated server and also upgrading the server software as needed.
                     </p>
+                    <h3>Apache &amp; Nginx</h3>
                     <p class="m-8 text-white">
                         The site runs on both Apache and Nginx due to the games tournament system requiring an old version of PHP and 
                         the rest of the site requiring a newer version of PHP as it uses Maxmind. I implemented Maxmind because we 
@@ -50,15 +64,20 @@
                         and started using newer versions of PHP which meant that we had to run the site on a newer version of PHP. But my 
                         directors never gave me the go ahead to update the tournament system to work with the newer version of PHP.
                         The tournament system and the games themselves are the only parts that I didn't create, although I supported both of 
-                        them and ended up fixing them when needed. Originally the games were Flash games and I co-ordinated with our graphics 
-                        designer to recreate images for HTML5 versions of the games and then hired freelance developers to convert all
-                        the games to HTML5 and create new HTML5 games. I was responsible for testing the games and putting the games live 
-                        on the website.
+                        them and ended up fixing them when needed. 
                     </p>
+
+                    <h3>CSS &amp; Javascript</h3>
+
+                    <h3>Maxmind</h3>
+
+                    <h3>Aweber</h3>
                     <p class="m-8 text-white">
                         The pages use server side includes (SSI) to include sectionsn based on geo location of the visitors. This allowed 
                         for the pages to still be HTML pages, which was a requirement for the site.
                     </p>
+
+                    <h3>Google PageSpeed Insights</h3>
                     <p class="m-8 text-white">
                         The site scores 90 and abave on Google PageSpeed Insights for every metric with most of them scoring 100 and is 
                         fully responsive.
@@ -97,6 +116,8 @@
                             class="px-4 py-2 bg-blue-900/30 rounded-lg border border-blue-700 text-blue-200">MySQL</span>
                         <span
                             class="px-4 py-2 bg-blue-900/30 rounded-lg border border-blue-700 text-blue-200"><a href="https://www.maxmind.com/en/home" target="_blank">MaxMind</a></span>
+                        <span
+                            class="px-4 py-2 bg-blue-900/30 rounded-lg border border-blue-700 text-blue-200"><a href="https://www.aweber.com/" target="_blank">Aweber</a></span>
                     </div>
                 </section>
 
@@ -105,8 +126,8 @@
                     <div class="grid md:grid-cols-2 gap-4">
                         <div class="p-4 bg-blue-900/30 rounded-lg border border-blue-800 text-white">Responsive Design</div>
                         <div class="p-4 bg-blue-900/30 rounded-lg border border-blue-800 text-white">User Registration and Authentication</div>
-                        <div class="p-4 bg-blue-900/30 rounded-lg border border-blue-800 text-white">Add &amp; Manage Property Listings</div>
-                        <div class="p-4 bg-blue-900/30 rounded-lg border border-blue-800 text-white">Filter Properties by Status</div>
+                        <div class="p-4 bg-blue-900/30 rounded-lg border border-blue-800 text-white">Geo Targeted Ads</div>
+                        <div class="p-4 bg-blue-900/30 rounded-lg border border-blue-800 text-white">In page HTML5 games</div>
                     </div>
                 </section>
 
